0f8c72ae0c1071a7af1210711fbcdef2cc94aa9e
Odecio Coutinho <odecio@purespectrum.com>
2024-12-30 13:33:28 -0300

    Using telescope

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,4 @@
 vim.opt.number = true -- Enable lines
 vim.opt.relativenumber = false -- Enables relative line numbers
 vim.b.autoformat = false --Disable format on save
+vim.g.lazyvim_picker = "telescope"
